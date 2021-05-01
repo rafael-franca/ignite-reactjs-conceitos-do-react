895253667d426848eaa4f9172b0587856c17968f
Rafael França <rafael.franca@ibyte.com.br>
2021-05-01 00:08:52 -0300

    [feat] handleRemoveTask

--- src/components/TaskList.tsx
+++ src/components/TaskList.tsx
@@ -37,7 +37,9 @@ export function TaskList() {
   }
 
   function handleRemoveTask(id: number) {
-    // Remova uma task da listagem pelo ID
+    const filteredTasks = tasks.filter(task => task.id !== id)
+
+    setTasks(filteredTasks)
   }
 
   return (
